2e89b88a0f1640bac2a8de08851c61b88cbeafef
hasecom <hasetennisth@gmail.com>
2020-07-06 14:48:51 +0900

    update

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
